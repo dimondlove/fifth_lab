ecdd8e053e1fde510337ba4c445140bb9bf7285d
Yuriy_Rudnev <rudnev2020@gmail.com>
2024-03-31 18:59:24 +0300

    Completed the task

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def form(request):
+    return render(request, "form_html.html")
+
+
+def post_student(request):
+    lastname = request.POST.get("lastname", "Undefined")
+    firstname = request.POST.get("firstname", "Undefined")
+    patronymic = request.POST.get("patronymic", "Undefined")
+    age = request.POST.get("age", 1)
+    address = request.POST.get("student-address", "Undefined")
+    student_group = request.POST.get("student_group", "Undefined")
+    subjects = request.POST.getlist("subjects", ["Undefined"])
+    data = {"lastname": lastname, "firstname": firstname, "patronymic": patronymic, "age": age, "address": address,
+            "student_group": student_group, "subjects": subjects}
+
+    return render(request, "student.html", context=data)
